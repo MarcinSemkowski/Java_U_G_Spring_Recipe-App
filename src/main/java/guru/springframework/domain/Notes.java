a2fe220c986c77cfa20e708e18ed03181b8982a0
Marcin <marcin96semkowski@gmail.com>
2019-07-31 17:34:12 +0200

    project Lombok Error #1 @Equals and HashCode

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,9 +1,11 @@
 package guru.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
     @Id
